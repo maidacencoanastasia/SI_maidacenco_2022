5279732fa5b7f91e3409e31d9063c1f2687427d9
maidacencoanastasia <maidacenco100@gmail.com>
2022-02-09 22:08:06 +0200

    second commit -- add input

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ numele = "Maidacenco Anastasia"
 def salutare(numele):
     return ("Hello, "+numele)
 print(salutare(numele))
+
+#Some changes
+name = input("What is you name?")
+print(salutare(name))
