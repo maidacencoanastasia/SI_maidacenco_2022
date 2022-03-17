0295b4d8a0c732e62e890b32099b54d29d1923b4
maidacencoanastasia <maidacenco100@gmail.com>
2022-03-17 08:51:14 +0200

    - homework

--- medical.py
+++ medical.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from imperio import BoxCoxTransformer, YeoJohnsonTransformer
 from kydavra import PValueSelector
 
-df = pd.read_csv("dataset/wine-quality-white-and-red.csv")
+df = pd.read_csv("dataset/heart.csv")
 print(df)
 df.info()
 # find the null values
@@ -13,4 +13,19 @@ null_v = df.isna().sum()
 print(null_v) # there are no null values
 
 print("----------------------------------")
-print(df['alcohol'].value_counts())
+#print(df['Age'].value_counts())
+
+plt.hist(df['Age'])
+plt.show()
+plt.hist(df['Sex'])
+plt.show()
+plt.hist(df['ChestPainType'])
+plt.show()
+plt.hist(df['Cholesterol'])
+plt.show()
+plt.hist(df['MaxHR'])
+plt.show()
+plt.hist(df['FastingBS'])
+plt.show()
+
+
